72e8d0a3df3d7c9d4f91ee9f33288dcf2db5eeee
GBLins14 <gabrielglins18@gmail.com>
2025-12-19 04:11:47 -0300

    refactor: DeliveryResponse.kt

--- src/main/kotlin/com/lobby/dto/DeliveryResponse.kt
+++ src/main/kotlin/com/lobby/dto/DeliveryResponse.kt
@@ -7,6 +7,7 @@ import java.time.Instant
 data class DeliveryResponseDto(
     val id: Long,
     val status: DeliveryStatus,
+    val trackingCode: String,
     val recipientName: String,
     val apartmentNumber: String?,
     val doormanId: Long,
@@ -17,6 +18,7 @@ fun Delivery.toResponse(): DeliveryResponseDto {
     return DeliveryResponseDto(
         id = this.id,
         status = this.status,
+        trackingCode = this.trackingCode,
         recipientName = this.recipientName,
         apartmentNumber = this.apartmentNumber,
         doormanId = this.doorman.id,
