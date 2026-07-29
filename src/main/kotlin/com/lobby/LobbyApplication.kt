0f16d22956bd3a1fb895314d6478f7838553de70
Gabriel Lins <119823460+GBLins14@users.noreply.github.com>
2026-07-29 05:18:59 -0300

    remove: dotenv
    Re-enable async support and simplify main function.

--- src/main/kotlin/com/lobby/LobbyApplication.kt
+++ src/main/kotlin/com/lobby/LobbyApplication.kt
@@ -5,18 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 
-@EnableAsync
 @SpringBootApplication
+@EnableAsync
 class LobbyApplication
-fun main(args: Array<String>) {
-    try {
-        val dotenv = Dotenv.configure().ignoreIfMissing().load()
-        dotenv.entries().forEach { entry ->
-            System.setProperty(entry.key, entry.value)
-        }
-    } catch (_: Exception) {
-        println("Arquivo .env não encontrado ou erro ao carregar. Usando variáveis de ambiente do sistema.")
-    }
 
-	runApplication<LobbyApplication>(*args)
+fun main(args: Array<String>) {
+    runApplication<LobbyApplication>(*args)
 }
