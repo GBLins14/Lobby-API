a8dd276cda645eb27f890bb336a2ef6b0158c1c8
Gabriel Lins <119823460+GBLins14@users.noreply.github.com>
2026-07-29 07:45:02 -0300

    Update AnalyticsController.kt

--- src/main/kotlin/com/lobby/controllers/AnalyticsController.kt
+++ src/main/kotlin/com/lobby/controllers/AnalyticsController.kt
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirements
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
